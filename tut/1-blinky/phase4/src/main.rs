1075a9e741e11c9ec19e26bc19b81397a0958aa4
Jesse Parsons <savvyjesse@aol.com>
2021-01-16 08:29:53 -0500

    feeble registers conquered, mastery of led achieved

--- tut/1-blinky/phase4/src/main.rs
+++ tut/1-blinky/phase4/src/main.rs
@@ -1,12 +1,11 @@
 #![feature(asm)]
 #![feature(global_asm)]
-
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
 #[cfg(not(test))]
 mod init;
-use core::ptr::write_volatile;
+use core::ptr::{write_volatile};
 
 const GPIO_BASE: usize = 0x3F000000 + 0x200000;
 
@@ -17,22 +16,23 @@ const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
 #[inline(never)]
 fn spin_sleep_ms(ms: usize) {
     for _ in 0..(ms * 6000) {
-        unsafe { asm!("nop" :::: "volatile"); }
+        unsafe {
+            asm!("nop" :::: "volatile");
+        }
     }
 }
 
 unsafe fn kmain() -> ! {
-    // use std::ptr::write_volatile;
     // FIXME: STEP 1: Set GPIO Pin 16 as output.
-    let val = 0b001u32;
-    // val << 17;
-    write_volatile(GPIO_FSEL1, val << 17);
-
-
+    write_volatile(GPIO_FSEL1, 0b0100_0000_0000_0000_0000u32);
     // FIXME: STEP 2: Continuously set and clear GPIO 16.
     loop {
-        write_volatile(GPIO_SET0, 0b10000u32); // 16 as binary 
-        spin_sleep_ms(1000);
-        write_volatile(GPIO_CLR0, 0b10000u32); // 16 as binary
+        // write a 1 to 16th position of the set0 register to output
+        write_volatile(GPIO_SET0, 0b1_0000_0000_0000_0000);
+        spin_sleep_ms(200);
+
+        // write a 1 to 16th position of the clr0 register to clear 
+        write_volatile(GPIO_CLR0, 0b1_0000_0000_0000_0000);
+        spin_sleep_ms(200);
     }
 }
